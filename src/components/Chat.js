c5b73fa6b823d19c08a8e8606a67b4d93a31be53
github username <github email address>
2020-07-07 19:23:45 -0500

    added icons

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,9 @@
 import React, { useRef, useEffect } from "react";
+import Kraber from "../images/Kraber-1.png";
+// import Wingman from "../images/Wingman-1.png";
+import Peacekeeper from "../images/Peacekeeper-1.png";
+import R99 from "../images/R99-1.png";
+import P2020 from "../images/P2020-1.png";
 
 export default function Chat({ messages }) {
   let chatContainer = useRef();
@@ -14,6 +19,19 @@ export default function Chat({ messages }) {
       return "mod";
     }
     return "none";
+  }
+
+  function assignImg(msg) {
+    if (msg.prefix.user === "gunchfps") {
+      return Kraber;
+    }
+    if (msg.tags.get("subscriber") === "1") {
+      return Peacekeeper;
+    }
+    if (msg.tags.get("mod") === "1") {
+      return R99;
+    }
+    return P2020;
   }
 
   let scrollToMyRef = () => {
@@ -35,8 +53,9 @@ export default function Chat({ messages }) {
             <p className="chat-message">
               <span className={assignClass(msg)}>
                 {msg.tags.get("display-name")}
-              </span>
-              {`:  ${msg.message.value}`}
+              </span>{" "}
+              <img src={assignImg(msg)} alt="wingman" />
+              {` ${msg.message.value}`}
             </p>
             <div className></div>
           </li>

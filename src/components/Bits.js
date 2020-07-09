ab94401d164f4b4767b627f0607d876b82e14fb6
github username <github email address>
2020-07-09 17:30:07 -0500

    added bits donation animation

--- src/components/Bits.js
+++ src/components/Bits.js
@@ -1,10 +1,15 @@
 import React from "react";
 import BitsImg from "../images/Diamond-1.png";
 
-export default function Bits({ bitCount }) {
-  console.log(bitCount);
+export default function Bits({ bitCount, index, bitDonationsLength }) {
   return (
-    <div className="bits-donation">
+    <div
+      className={
+        index === bitDonationsLength - 1
+          ? "latest-bits-donation"
+          : "bits-donation"
+      }
+    >
       <img src={BitsImg} alt="bits" /> {bitCount}
     </div>
   );
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -43,7 +43,7 @@ export default function Chat({ messages }) {
   useEffect(() => {
     scrollToBottomOfChat();
   }, [messages]);
-  console.log(messages[messages.length - 1]);
+  // console.log(messages[messages.length - 1]);
   return (
     <ol ref={chatContainer} className="chat-container">
       {messages.map((msg) => {
